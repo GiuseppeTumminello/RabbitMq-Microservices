d8519bcc849b9c237a93a4d6ab727cc4ffd3a3c1
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-25 08:38:51 +0200

    removed annotation service from interfaces

--- PensionZus/src/main/java/com/acoustic/service/SalaryCalculatorService.java
+++ PensionZus/src/main/java/com/acoustic/service/SalaryCalculatorService.java
@@ -1,13 +1,12 @@
 package com.acoustic.service;
 
 import com.acoustic.entity.PensionZus;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.function.UnaryOperator;
 
 
-@Service
+
 public interface SalaryCalculatorService extends UnaryOperator<BigDecimal> {
 
      String getDescription();
--- Tax/src/main/java/com/acoustic/service/SalaryCalculatorService.java
+++ Tax/src/main/java/com/acoustic/service/SalaryCalculatorService.java
@@ -1,13 +1,12 @@
 package com.acoustic.service;
 
 import com.acoustic.entity.Tax;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.function.UnaryOperator;
 
 
-@Service
+
 public interface SalaryCalculatorService extends UnaryOperator<BigDecimal> {
 
     String getDescription();
